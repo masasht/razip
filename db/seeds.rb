9d7fdb65a91e4380d0e16abe5e8b33b5f75c86b7
kano-pon <masasht@gmail.com>
2018-02-04 14:17:02 +0000

    params.require supply_servo

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,15 +16,4 @@ require "csv"
 #              turn: row[7], kv: row[8], rpm: row[9], torque: row[10], voltage: row[11], ampere: row[12], information: row[13])
 #end
 
-FrequencySelection.create(frequency_type: 'FASST（フタバ）')
-FrequencySelection.create(frequency_type: 'T-FHSS（フタバ）')
-FrequencySelection.create(frequency_type: 'S-FHSS（フタバ）')
-FrequencySelection.create(frequency_type: 'FHSS（フタバ）')
-FrequencySelection.create(frequency_type: 'FH4（サンワ）')
-FrequencySelection.create(frequency_type: 'FH3（サンワ）')
-FrequencySelection.create(frequency_type: 'DSSS（サンワ）')
-FrequencySelection.create(frequency_type: 'Dual FHSS（KO）')
-FrequencySelection.create(frequency_type: 'Single FHSS（KO）')
-FrequencySelection.create(frequency_type: 'Gyro FHSS（KO）')
-FrequencySelection.create(frequency_type: 'DSSS（KO）')
-FrequencySelection.create(frequency_type: 'LYNX 4S/LYNX 4S PLUS専用（HiTEC）')
+
