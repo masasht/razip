dd68bdff33476f87f4d0512593ee68db192adcbe
kano-pon <masasht@gmail.com>
2018-02-04 07:52:34 +0000

    test

--- db/migrate/20180120051703_change_datatype_turn_motors.rb
+++ db/migrate/20180120051703_change_datatype_turn_motors.rb
@@ -1,5 +1,14 @@
 class ChangeDatatypeTurnMotors < ActiveRecord::Migration[5.0]
   def change
-    change_column :motors, :turn, "USING turn::double precision"
+    
+    # 変更内容
+    def up
+      change_column :motors, :turn, :float 
+    end
+    
+    # 変更前の状態
+    def down
+      change_column :motors, :turn, :string
+    end
   end
 end
